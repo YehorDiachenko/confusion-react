5b59f472dd2fe43e5f8c8d4b296a317b57357cc8
YehorDiachenko <diachenko.yehor@gmail.com>
2020-06-12 20:32:18 +0300

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
           <NavbarBrand href="/">Restaurant Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
