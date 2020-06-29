5d7bdf4dd1aecb127352b83bc9435fabe581b656
YehorDiachenko <diachenko.yehor@gmail.com>
2020-06-29 19:05:55 +0300

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle,
+          Breadcrumb, BreadcrumbItem, Button,
+          Modal, ModalHeader, ModalBody,
+          Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
 
   function RenderComments({ comments }) {
@@ -23,6 +30,7 @@ import { Link } from 'react-router-dom';
               );
             })}
           </ul>
+          <CommentForm />
         </div>
       );
     } else {
@@ -74,6 +82,91 @@ const DishDetail = (props) => {
         </div>
       </div>
     );
+}
+
+class CommentForm extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      isModalOpen: false
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleSubmit(values) {
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
+    // event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <Button outline color="secondary" onClick={this.toggleModal}><span className='fa fa-pencil'></span> Submit Comment</Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Row className="form-group">
+                <Label htmlFor="rating" md="12">Rating</Label>
+                <Col md="12">
+                  <Control.select model=".rating" name="rating" className="form-control">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                  </Control.select>
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Label htmlFor="author" md="12">Your Name</Label>
+                <Col md="12">
+                  <Control.text model=".author" id="author" name="author"
+                      placeholder="Your Name"
+                      className="form-control"
+                      validators={{required, minLength: minLength(3), maxLength: maxLength(15)}}
+                  />
+                  <Errors className="text-danger"
+                        model=".author"
+                        show="touched"
+                        messages={{
+                            required: 'Required! ',
+                            minLength: 'Must be greater than 2 characters',
+                            maxLength: 'Must be 15 characters or less'}}
+                  />
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Label htmlFor="comment" md="12">Comment</Label>
+                <Col md="12">
+                  <Control.textarea model=".comment" id="comment" name="comment"
+                    rows="6" className="form-control" />
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Col md="12">
+                  <Button color="primary" type="submit">Submit</Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default DishDetail;
